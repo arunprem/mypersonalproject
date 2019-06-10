4958da0c5f81c1fb5bbca67f3d12ed0383c1abe3
Arunprem <kuttunick@gmail.com>
2019-06-10 17:40:31 +0530

    completed integration of google map

--- lib/pages/nearest_ps.dart
+++ lib/pages/nearest_ps.dart
@@ -3,9 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 import 'package:location/location.dart';
-import 'package:flutter/services.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Nearest_ps extends StatefulWidget {
   @override
@@ -19,7 +18,6 @@ class _Nearest_psState extends State<Nearest_ps> {
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-
     target: LatLng(10.8505, 76.2711),
     zoom: 6.4746,
   );
@@ -30,12 +28,8 @@ class _Nearest_psState extends State<Nearest_ps> {
       tilt: 10.440717697143555,
       zoom: 19.151926040649414);
 
-
-
-  
- @override
+  @override
   void initState() {
-
     super.initState();
     _getLocation().then((value) {
       setState(() {
@@ -46,15 +40,16 @@ class _Nearest_psState extends State<Nearest_ps> {
 
   @override
   Widget build(BuildContext context) {
-
-
-
     return new Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey,
-          title: new Text('Nearest Police Station'),
-        ),
-      body: GoogleMap(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: new Text('Nearest Police Station'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          _googleMap(context)
+        ],
+      ) /*GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
@@ -65,12 +60,12 @@ class _Nearest_psState extends State<Nearest_ps> {
         onPressed: _goToTheLake,
         label: Text('Go To PS!'),
         icon: Icon(Icons.directions_boat),
-      ),
+      ),*/
     );
-
   }
+
   Future<void> _goToTheLake() async {
-  // debugPrint(_kLake.toString());
+    // debugPrint(_kLake.toString());
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
@@ -84,4 +79,28 @@ class _Nearest_psState extends State<Nearest_ps> {
     }
     return currentLocation;
   }
+
+  Widget _googleMap(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition:
+              CameraPosition(target: LatLng(8.5095, 76.9568), zoom: 12),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete();
+          },
+          markers: {psMarker}),
+    );
+  }
 }
+
+Marker psMarker = Marker(
+  markerId: MarkerId("Museam Police Station"),
+  position: LatLng(8.5095, 76.9568),
+  infoWindow: InfoWindow(title: "Museam Police Station"),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+     BitmapDescriptor.hueBlue
+  )
+);
