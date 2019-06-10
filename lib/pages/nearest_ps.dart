77d61260aa4ad2519dd92aa61911efc37c619957
Arunprem <kuttunick@gmail.com>
2019-06-10 13:44:42 +0530

    commit dialacop

--- lib/pages/nearest_ps.dart
+++ lib/pages/nearest_ps.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import 'package:location/location.dart';
 import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Nearest_ps extends StatefulWidget {
   @override
@@ -12,6 +13,20 @@ class Nearest_ps extends StatefulWidget {
 
 class _Nearest_psState extends State<Nearest_ps> {
   var location = new Location();
+
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
 
   Map<String, double> userLocation;
   
@@ -28,44 +43,25 @@ class _Nearest_psState extends State<Nearest_ps> {
 
   @override
   Widget build(BuildContext context) {
+
+
+
     return new Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blueGrey,
           title: new Text('Nearest Police Station'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              userLocation == null
-                  ? CircularProgressIndicator()
-                  : Text("Location is " +
-                      "Latitude :" +
-                      userLocation["latitude"].toString() +
-                      " " +
-                      " Longitude " +
-                      userLocation["longitude"].toString()),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RaisedButton(
-                  onPressed: () {
-                    _getLocation().then((value) {
-                      setState(() {
-                        userLocation = value;
-                      });
-                    });
-                  },
-                  color: Colors.blueGrey,
-                  child: Text(
-                    "Get Location",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ));
+      body:GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
+
   }
+
 
   Future<Map<String, double>> _getLocation() async {
     var currentLocation = <String, double>{};
